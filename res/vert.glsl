587139303e59eeed5027aad484a24f4c7dc7b7b0
Tyler Tesch <tytesch@gmail.com>
2017-03-03 09:24:35 -0600

    Perspective working correctly

--- res/vert.glsl
+++ res/vert.glsl
@@ -6,8 +6,9 @@ in vec3 color;
 out vec3 f_color;
 
 uniform mat4 P;
+uniform mat4 M;
 
 void main() {
-    gl_Position = P * vec4(pos, 1.0);
+    gl_Position = P * M * vec4(pos, 1.0);
     f_color = color;
 }
--- src/main.rs
+++ src/main.rs
@@ -104,15 +104,23 @@ fn main() {
     
     let prog = Program::from_source(&display, vert_shader, frag_shader, None).unwrap();
 
+    let mut zoom: f32 = 0.8;
     loop {
         let mut target = display.draw();
-
         let (width, height) = target.get_dimensions();
-        let p = create_perspective(width, height, 0.1, 1024.0);
+
+        let p = create_perspective(width, height);
+
+        let m = [
+            [zoom, 0.0, 0.0, 0.0],
+            [0.0, zoom, 0.0, 0.0],
+            [0.0, 0.0, zoom, 0.0],
+            [0.0, 0.0, 2.0, 1.0f32],
+        ];
 
         target.clear_color(0.0, 0.0, 0.0, 1.0);
 
-        target.draw(&v_buffer, &indices, &prog, &uniform! { P: p }, &Default::default()).unwrap();
+        target.draw(&v_buffer, &indices, &prog, &uniform! { P: p, M: m }, &Default::default()).unwrap();
         
         target.finish().unwrap();
 
@@ -125,15 +133,17 @@ fn main() {
     }
 }
 
-fn create_perspective(width: u32, height: u32, zfar: f32, znear: f32) -> [[f32; 4]; 4] {
+fn create_perspective(width: u32, height: u32) -> [[f32; 4]; 4] {
     let aspect_ratio = height as f32 / width as f32;
 
     let fov: f32 = 3.141592 / 3.0;
+    let znear = 0.1;
+    let zfar = 1024.0;
 
     let f = 1.0 / (fov / 2.0).tan();
 
     [
-        [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
+        [  f  *  aspect_ratio ,    0.0,              0.0              ,   0.0],
         [         0.0         ,     f ,              0.0              ,   0.0],
         [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
         [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
